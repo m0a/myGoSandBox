4d34e8bb007d558f9585b74830fc33e2fee37d52
makoto abe <abe00makoto@gmail.com>
2014-09-09 16:01:36 +0900

    go http study

--- myserver.go
+++ myserver.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +16,23 @@ func name(rw http.ResponseWriter, req *http.Request) {
 
 func main() {
 
-	http.HandleFunc("/", IndexHandler)
-	http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		path = strings.Replace(path, "/", " ", -1)
+		fmt.Fprintf(w, "Hello! %s", path)
+
+	})
+
+	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		queryParam := r.URL.Query()
+		name := queryParam.Get("name")
+		age := queryParam.Get("age")
+		fmt.Printf("%#v\n\n", r)
+		fmt.Printf("%#v\n", r.URL)
+
+		fmt.Fprintf(w, "Hello! %s,your age is %s", name, age)
+	})
+
+	http.ListenAndServe(":4000", nil)
 
 }
